59005685ad1371d99b7a96d62d06f87acb8af464
CHAE YEON SUNG <ch.yeondev@gmail.com>
2024-11-06 23:45:13 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 792 ms, Memory: 110724 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
@@ -7,9 +8,37 @@ public class Main {
 
         final String str = br.readLine();
 
+        Map<Character, Integer> charToIndex = new HashMap();
+        charToIndex.put('a', 1);
+        charToIndex.put('b', 2);
+        charToIndex.put('c', 3);
+        charToIndex.put('d', 4);
+        charToIndex.put('e', 5);
+        charToIndex.put('f', 6);
+        charToIndex.put('g', 7);
+        charToIndex.put('h', 8);
+        charToIndex.put('i', 9);
+        charToIndex.put('j', 10);
+        charToIndex.put('k', 11);
+        charToIndex.put('l', 12);
+        charToIndex.put('m', 13);
+        charToIndex.put('n', 14);
+        charToIndex.put('o', 15);
+        charToIndex.put('p', 16);
+        charToIndex.put('q', 17);
+        charToIndex.put('r', 18);
+        charToIndex.put('s', 19);
+        charToIndex.put('t', 20);
+        charToIndex.put('u', 21);
+        charToIndex.put('v', 22);
+        charToIndex.put('w', 23);
+        charToIndex.put('x', 24);
+        charToIndex.put('y', 25);
+        charToIndex.put('z', 26);
+
         int[][] map = new int[26+1][str.length()+1];
         for(int i = 1; i <= str.length(); i++) {
-            final int index = str.charAt(i - 1) - 'a' + 1;
+            int index = charToIndex.get(str.charAt(i - 1));
             map[index][i] = map[index][i-1] + 1;
             for(int k = 1; k <= 26; k++) {
                 if(k != index) map[k][i] = map[k][i-1];
@@ -24,13 +53,15 @@ public class Main {
             char alphabet = line[0].charAt(0);
             final int start = Integer.parseInt(line[1]);
             final int end = Integer.parseInt(line[2]);
-
-            result[i] = map[alphabet - 'a' + 1][end+1] - map[alphabet - 'a' + 1][start];
+            int index = charToIndex.get(alphabet);
+            result[i] = map[index][end+1] - map[index][start];
         }
 
+        StringBuilder sb = new StringBuilder();
         for(int res : result) {
-            System.out.println(res);
+            sb.append(res).append("\n");
         }
+        System.out.println(sb);
     }
 
 }
