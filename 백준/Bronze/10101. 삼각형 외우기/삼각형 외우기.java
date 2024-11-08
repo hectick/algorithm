40100a6307caeaa611a2e67504bb1378b1e21137
CHAE YEON SUNG <ch.yeondev@gmail.com>
2024-11-08 23:29:08 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 100 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        final int a = Integer.parseInt(br.readLine());
+        final int b = Integer.parseInt(br.readLine());
+        final int c = Integer.parseInt(br.readLine());
+
+        if(a == 60 && b == 60 && c == 60) {
+            System.out.println("Equilateral");
+        }else if(a + b + c != 180) {
+            System.out.println("Error");
+        }else if(a == b || b == c || c == a) {
+            System.out.println("Isosceles");
+        }else{
+            System.out.println("Scalene");
+        }
+    }
+
+}
